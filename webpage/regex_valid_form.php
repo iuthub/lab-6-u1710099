9861c734820be134c6997adb7b60a00d7e537da8
u1710099 <47207177+u1710099@users.noreply.github.com>
2019-03-23 18:05:44 +0500

    Add files via upload

--- webpage/regex_valid_form.php
+++ webpage/regex_valid_form.php
@@ -1,29 +1,34 @@
-<?php
-
+<?php 
+// .{2,}   2 more chars
 	$pattern="";
 	$text="";
 	$replaceText="";
-	$replacedText="";
-
+    $replaced_text = ""; 
 	$match="Not checked yet.";
 
+
 if ($_SERVER["REQUEST_METHOD"]=="POST") {
+	
+
 	$pattern=$_POST["pattern"];
 	$text=$_POST["text"];
 	$replaceText=$_POST["replaceText"];
 
-	$replacedText=preg_replace($pattern, $replaceText, $text);
+	$replaced_text = preg_replace($pattern, $text, $replaceText);
+
+	preg_match($pattern, $text, $matched);
 
 	if(preg_match($pattern, $text)) {
 						$match="Match!";
+						$replaced_text = $matched[1];
+						print_r($matched);
 					} else {
 						$match="Does not match!";
-					}
+	//$not_digit = "[^\d\]";				
+	}
+    
 }
-
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -35,23 +40,22 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
 		<dl>
 			<dt>Pattern</dt>
 			<dd><input type="text" name="pattern" value="<?= $pattern ?>"></dd>
-
+			
 			<dt>Text</dt>
 			<dd><input type="text" name="text" value="<?= $text ?>"></dd>
-
+			
 			<dt>Replace Text</dt>
 			<dd><input type="text" name="replaceText" value="<?= $replaceText ?>"></dd>
 
 			<dt>Output Text</dt>
 			<dd><?=	$match ?></dd>
 
-			<dt>Replaced Text</dt>
-			<dd> <code><?=	$replacedText ?></code></dd>
-
+			<dt>Output Text</dt>
+			<dd><?=	$replaced_text ?></dd>
 			<dt>&nbsp;</dt>
 			<dd><input type="submit" value="Check"></dd>
 		</dl>
-
+		
 	</form>
 </body>
 </html>
